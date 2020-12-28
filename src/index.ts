ef47966e4ca3866c13d85c5752b57458b468ceed
Richard Ng <richardcrng@gmail.com>
2020-12-28 13:03:45 +0000

    Use random-words library

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { question as prompt } from "readline-sync";
+import randomWords from "random-words";
 
 interface LetterSlot {
   letter: string;
   isRevealed?: boolean;
 }
 
+let hiddenWord: string;
 let board: LetterSlot[];
 let livesRemaining: number = 5;
 
@@ -15,25 +17,43 @@ function boardContainsLetter(letterToCheck: string): boolean {
   return board.some((letterSlot) => letterSlot.letter === letterToCheck);
 }
 
-function boardForPrint(): string[] {
-  return board.map(letterSlotForPrint);
-}
-
-function handleGuess(guess: string): void {
-  storeGuess(guess);
-  if (boardContainsLetter(guess)) {
-    console.log("Bullseye!");
-    revealLetter(guess);
-  } else {
-    console.log("It's not there...");
-    livesRemaining -= 1;
-  }
+function boardForPrint(): string {
+  return board.map(letterSlotForPrint).join(" ");
 }
 
 function gameIsOngoing(): boolean {
   return (
     livesRemaining > 0 && board.some((letterSlot) => !letterSlot.isRevealed)
   );
+}
+
+function handleGuess(guess: string): void {
+  storeGuess(guess);
+  if (guess.length === 1) {
+    processGuessedLetter(guess);
+  } else {
+    processGuessedWord(guess);
+  }
+}
+
+function processGuessedLetter(letter: string): void {
+  if (boardContainsLetter(letter)) {
+    console.log("Bullseye!");
+    revealLetter(letter);
+  } else {
+    console.log("It's not there...");
+    livesRemaining -= 1;
+  }
+}
+
+function processGuessedWord(word: string): void {
+  if (word === hiddenWord) {
+    console.log("Bullseye!");
+    revealWholeBoard();
+  } else {
+    console.log("That's not the word...");
+    livesRemaining -= 1;
+  }
 }
 
 function letterSlotForPrint(letterSlot: LetterSlot): string {
@@ -46,8 +66,8 @@ function letterSlotForPrint(letterSlot: LetterSlot): string {
 
 function playGame(): void {
   console.log("Let's play hangman!");
-  const word = "apple";
-  board = word.split("").map((char) => ({ letter: char }));
+  hiddenWord = randomWords();
+  board = hiddenWord.split("").map((char) => ({ letter: char }));
   while (gameIsOngoing()) {
     playRound();
   }
@@ -57,7 +77,7 @@ function playGame(): void {
   } else {
     console.log("Congrats! You won!");
   }
-  console.log("The word was", word.toUpperCase());
+  console.log("The word was", hiddenWord.toUpperCase());
 }
 
 function playRound(): void {
@@ -65,6 +85,12 @@ function playRound(): void {
   console.log("Your lives remaining:", livesRemaining);
   console.log("Your previous guesses:", guessesMade);
   takeAndHandleGuess();
+}
+
+function revealWholeBoard(): void {
+  for (const letterSlot of board) {
+    letterSlot.isRevealed = true;
+  }
 }
 
 function revealLetter(letterToReveal: string): void {
